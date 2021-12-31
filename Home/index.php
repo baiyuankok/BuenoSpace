935a442ec4c0bc77a19ac848987fec2150150992
liang14 <tcliang0714@gmail.com>
2021-12-31 23:38:16 +0800

    updated space detail page and upload new js file for header

--- Home/index.php
+++ Home/index.php
@@ -1,6 +1,6 @@
 <?php
 // Connect to database
-require_once "config.php";
+require_once "../Home/config.php";
 require "../Home/main_page.html";
 
 function select_images_id($pdo, $space) {
@@ -14,8 +14,23 @@ function select_details($pdo, $space) {
 }
 
 // Select popular spaces
-$select_space = "SELECT spaceID, count(*) FROM myfirstdatabase.favourite GROUP BY spaceID ORDER BY count(*) DESC LIMIT 3";
+$select_space = "SELECT spaceID, count(*) FROM myfirstdatabase.booking GROUP BY spaceID ORDER BY count(*) DESC LIMIT 3";
 $space_id = $pdo->query($select_space)->fetchAll(PDO::FETCH_COLUMN, 0);
+
+// If the space from booking table is less than 3
+if (count($space_id) < 3) {
+    $more_space_id = "SELECT spaceID FROM myfirstdatabase.space WHERE spaceID NOT IN (";
+    foreach ($space_id as $each_more_space) {
+        $more_space_id .= $each_more_space.",";
+    }
+    $more_space_id = substr_replace($more_space_id, ")", -1);
+    // Calculate the number of space left to be rendered
+    $left_space = 3 - count($space_id);
+    $more_space_id .= " LIMIT ".$left_space;
+    // Get the remaining spaces to be rendered
+    $more_space_result = $pdo->query($more_space_id)->fetchAll(PDO::FETCH_COLUMN, 0);
+    $space_id = array_merge($space_id, $more_space_result);
+}
 // Select space images and details
 $counter = 1;
 foreach($space_id as $each_space) {
@@ -24,6 +39,7 @@ foreach($space_id as $each_space) {
     foreach ($img_id as $each_img) {
         echo '<script>
                 var imgSec = document.getElementById("space-image-'.$counter.'");
+                imgSec.classList.add("database-space-id-'.$each_space.'");
                 var imgEle = document.createElement("IMG");
                 imgEle.setAttribute("data-src", "image.php?id='.$each_img.'");
                 imgEle.classList.add("each-img");
