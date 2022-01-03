6cf428f7e6b89ef63f6b0f0bd94d3be251c975ba
AshleyWang <ashleywangsm013@gmail.com>
2022-01-03 16:47:06 +0800

    added read from database function

--- Search/search_results_logic.php
+++ Search/search_results_logic.php
@@ -1,14 +1,16 @@
 <?php
 
 //Connect to database
-require_once "../Home/config.php"
+require_once "../Home/config.php";
 require "../Search/search_results_page.html";
 
 // $get_space_id = $_GET['spaceID'];
 
+//Get all the space IDs
 // TOD0: modify the below line to become a search query
 $select_space = "SELECT spaceID FROM myfirstdatabase.space LIMIT 6";
 $space_id = $pdo->query($select_space)->fetchAll(PDO::FETCH_COLUMN, 0);
+
 
 
 function select_images_id($pdo, $space) {
@@ -21,11 +23,27 @@ function select_details($pdo, $space) {
     return $results->fetch();
 }
 
+
+
 $counter = 1;
 foreach($space_id as $each_space) {
-    // $img_id = select_images_id($pdo, $each_space);
-    // $img_counter = 1;
-    // foreach()
+    $img_id = select_images_id($pdo, $each_space);
+    $img_counter = 1;
+    foreach($img_id as $each_img) {
+        echo '<script>
+            var imgSec = document.getElementById("space-image-'.$counter.'");
+            imgSec.classList.add("database-space-id-'.$each_space.'");
+            var imgEle = document.createElement("IMG");
+            imgEle.setAttribute("data-src", "../Home/image.php?id='.$each_img.'");
+            imgEle.classList.add("each-img");
+            imgEle.alt = "space-'.$counter.'-'.$img_counter.'";
+            imgSec.appendChild(imgEle);
+        </script>';
+        $img_counter += 1;
+    }
+
+
+
     $each_detail = select_details($pdo, $each_space);
     echo '<script>
             document.getElementById("space-name-'.$counter.'").innerHTML = "'.$each_detail['name'].'";
@@ -33,7 +51,7 @@ foreach($space_id as $each_space) {
             document.getElementById("space-price-'.$counter.'").innerHTML = "'.$each_detail['price'].'";
             document.getElementById("space-capacity-'.$counter.'").innerHTML = "'.$each_detail['capacity'].'";
         </script>';
-    $counter +=1;
+    $counter += 1;
 }
 
 ?>
