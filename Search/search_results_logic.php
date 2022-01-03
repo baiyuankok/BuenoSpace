c0eac7afa04b049ac50ebd94470caaa51115be95
AshleyWang <ashleywangsm013@gmail.com>
2022-01-03 23:56:48 +0800

    added sql search queries

--- Search/search_results_logic.php
+++ Search/search_results_logic.php
@@ -19,9 +19,15 @@ echo '<html><br></html>';
 //Get event type, location, minimum price (if it exists), maximum price (if it exists) and capacity (if it exists)
 $event_type = $_POST["events"];
 $location = $_POST["location"];
-if (isset($_POST["min_price"])) $min_price = $_POST["min_price"];
-if (isset($_POST["max_price"])) $max_price = $_POST["max_price"];
-if (isset($_POST["capacity"])) $capacity = $_POST["capacity"];
+if (isset($_POST["min_price"])) {
+    $min_price = $_POST["min_price"];
+} else $min_price = NULL;
+if (isset($_POST["max_price"])) {
+    $max_price = $_POST["max_price"];
+} else $max_price = NULL;
+if (isset($_POST["capacity"])) {
+    $capacity = $_POST["capacity"] ;
+} else $capacity = NULL;
 
 //Test to see if variables were initialized
 echo $event_type;
@@ -36,10 +42,61 @@ echo $capacity;
 
 
 //Get all the space IDs
-// TOD0: modify the below line to become a search query
+//Default: if no location or event type is selected, display all
 $select_space = "SELECT spaceID FROM myfirstdatabase.space LIMIT 6";
 $space_id = $pdo->query($select_space)->fetchAll(PDO::FETCH_COLUMN, 0);
 
+//Search queries
+if ($location != "all") {
+    //If location is selected
+    $query = "SELECT spaceID FROM myfirstdatabase.space WHERE location = '$location'";
+    $space_id = $pdo->query($query)->fetchAll(PDO::FETCH_COLUMN, 0);
+}
+elseif ($event_type != "all-events") {
+    //If event type is selected
+    $query = "SELECT spaceID 
+    FROM myfirstdatabase.space t1
+    INNER JOIN myfirstdatabase.event_space_type t2
+    ON t1.spaceID = t2.space_ID
+    INNER JOIN myfirstdatabase.event_type t3
+    ON t2.event_type_ID = t3.event_type_ID
+    WHERE t3.type = '$event_type'";
+    $space_id = $pdo->query($query)->fetchAll(PDO::FETCH_COLUMN, 0);
+}
+elseif (($location != "all")&&($event_type != "all-events")) {
+    //If location & event type are selected
+    $query = "SELECT spaceID 
+    FROM myfirstdatabase.space t1
+    INNER JOIN myfirstdatabase.event_space_type t2
+    ON t1.spaceID = t2.space_ID
+    INNER JOIN myfirstdatabase.event_type t3
+    ON t2.event_type_ID = t3.event_type_ID
+    WHERE t1.location = '$location'
+    AND t3.type = '$event_type'";
+    $space_id = $pdo->query($query)->fetchAll(PDO::FETCH_COLUMN, 0);
+}
+elseif (($location != "all")&&($event_type != "all-events")&&($min_price != NULL)&&($capacity != NULL)) {
+    //If location, event type, price & capacity are selected
+    $query = "SELECT spaceID 
+    FROM myfirstdatabase.space t1
+    INNER JOIN myfirstdatabase.event_space_type t2
+    ON t1.spaceID = t2.space_ID
+    INNER JOIN myfirstdatabase.event_type t3
+    ON t2.event_type_ID = t3.event_type_ID
+    WHERE t1.location = '$location'
+    AND t3.type = '$event_type'
+    AND t1.price BETWEEN $min_price AND $max_price
+    AND t1.capacity BETWEEN $capacity-5 AND $capacity+5";
+    $space_id = $pdo->query($query)->fetchAll(PDO::FETCH_COLUMN, 0);
+}
+elseif (($location == "all")&&($event_type == "all-events")&&($min_price != NULL)&&($capacity != NULL)) {
+    //If price & capacity are selected
+    $query = "SELECT spaceID 
+    FROM myfirstdatabase.space
+    WHERE price BETWEEN $min_price AND $max_price
+    AND capacity BETWEEN $capacity-5 AND $capacity+5";
+    $space_id = $pdo->query($query)->fetchAll(PDO::FETCH_COLUMN, 0);
+}
 
 
 function select_images_id($pdo, $space) {
