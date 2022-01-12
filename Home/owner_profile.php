b51db4e63dd57b7f289c6b7bab1c9ed84af7eab2
Kok Bai Yuan <baiyuan100722@student.usm.my>
2022-01-13 01:45:39 +0800

    modify indention

--- Home/owner_profile.php
+++ Home/owner_profile.php
@@ -32,118 +32,96 @@
 
 <body>
 <header id="header"></header>
-    
-
-
     <section id="profile_page_layout">
-
         <div class="profile_infor">
-           
             <div id="information_edit">
-                
-            <h5>WELCOME - <?php echo $owner_name; ?></h5>
-
-            <br>
-            <form class="profile_form" action="" method="post">
-
-                <label>Name</label>
-                <input type="text" name="owner_name"  value="<?php echo $owner_name; ?>">
+                <h5>WELCOME - <?php echo $owner_name; ?></h5>
                 <br>
-                <span class="help-block"><?php echo $owner_name_err; ?></span>
-                <br>
+                <form class="profile_form" action="" method="post">
+                    <label>Name</label>
+                    <input type="text" name="owner_name"  value="<?php echo $owner_name; ?>">
+                    <br>
+                    <span class="help-block"><?php echo $owner_name_err; ?></span>
+                    <br>
 
-                <label for="password">Email</label>
-                <input type="text" id="password" name="owner_email" value="<?php echo $owner_email; ?>">
-                <br>
-                <span class="help-block"><?php echo $owner_email_err; ?></span>
-                <br>
+                    <label for="password">Email</label>
+                    <input type="text" id="password" name="owner_email" value="<?php echo $owner_email; ?>">
+                    <br>
+                    <span class="help-block"><?php echo $owner_email_err; ?></span>
+                    <br>
 
-                <label for="password">Contact Number</label>
-                <input type="text" id="password" name="owner_contact" value="<?php echo $owner_contact; ?>">
-                <br>
-                <span class="help-block"><?php echo $owner_contact_err; ?></span>
-                <br><br>
+                    <label for="password">Contact Number</label>
+                    <input type="text" id="password" name="owner_contact" value="<?php echo $owner_contact; ?>">
+                    <br>
+                    <span class="help-block"><?php echo $owner_contact_err; ?></span>
+                    <br><br>
 
-                <div class="form-btn">
-                    <input type="submit" class="small-btn" name="edit_owner_profile" value="Save" onclick="fade_out()">
-                    <p id="success_msg"><?php echo $updatedMsg; ?></p>
-                </div>
+                    <div class="form-btn">
+                        <input type="submit" class="small-btn" name="edit_owner_profile" value="Save" onclick="fade_out()">
+                        <p id="success_msg"><?php echo $updatedMsg; ?></p>
+                    </div>
                 </form>
                 <br><br>
-                            </div>
-        </div>
-           
-        </section>
+            </div>
+        </div>   
+    </section>
 
-    
-    
-        <br><br><br>
-
-        <section id="profile_page_layout">
-            <h5 style="display: inline;">Space Listing</h5>
-            <i class="fas fa-plus" style="display: inline; cursor:pointer; margin-left: 10px" onclick="addSpace()"></i>
-            <span id="query-result-msg" style="margin-left: 10px; display: inline; opacity: 0.85;"><?php echo $query_msg; ?></span>
+    <!-- <br><br><br> -->
+    <section id="table_layout">
         <br><br>
+        <table>
+            <h5 class="tableName">Space Listing <i class="fas fa-plus" style="display: inline; cursor:pointer; margin-left: 10px" onclick="addSpace()"></i><span id="query-result-msg" style="margin-left: 10px; display: inline; opacity: 0.85;"><?php echo $query_msg; ?></span></h5>
+            <thead>
+                <tr>
+                    <th>Space</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    //find the owner space listing and put it in table view
+                    $sql = "SELECT * FROM space where ownerID=$ownerID";
+                    //run the sql query
+                    $pdoQuery_run= $pdo->query($sql);
+                    //if query run
+                    if ($pdoQuery_run) {
+                        //fetch pdo object
+                        while ($row= $pdoQuery_run->fetch(PDO::FETCH_OBJ)) {
+                            ////found owner record in space table
+                            $name= $row->name; 
+                            $space_id = $row->spaceID; ?>
 
-            <table>
-        <tr>
-            <th>Space</th>
-            <th></th>
-            <th></th>
-           
-        </tr>
+                            <tr>
+                                <td><?php echo $name; ?></td>
+                                <td><a href="../Spaces/space_listing.php?spaceID=<?php echo $space_id; ?>">Edit</a></td>
+                                <td><a href="../Spaces/space_deleting.php?spaceID=<?php echo $space_id; ?>">Delete</a></td>
+                            </tr>
+                <?php }}
+                    else{
+                        echo 'not found data';
+                    }
 
-        <?php
-       
-            //find the owner space listing and put it in table view
-             $sql = "SELECT * FROM space where ownerID=$ownerID";
-             //run the sql query
-             $pdoQuery_run= $pdo->query($sql);
-             //if query run
-             if ($pdoQuery_run) {
-              //fetch pdo object
-              while ($row= $pdoQuery_run->fetch(PDO::FETCH_OBJ)) {
-                  ////found owner record in space table
-                 $name= $row->name; 
-                 $space_id = $row->spaceID; ?>
-
-                    <tr>
-                        <td><?php echo $name; ?></td>
-                        <td><a href="../Spaces/space_listing.php?spaceID=<?php echo $space_id; ?>">Edit</a></td>
-                        <td><a href="../Spaces/space_deleting.php?spaceID=<?php echo $space_id; ?>">Delete</a></td>
-                    </tr>
-
-           <?php  }}
-             else{
-                  echo 'not found data';
-             }
-
-
-             //check if there is space listed by owner. if no then echo 'no space listed' in table row
-                $countRecord="SELECT * FROM space WHERE ownerID=$ownerID";
-                $countRecord= $pdo->query($countRecord);
-                $countRecord=$countRecord->rowCount();
-                
-                if($countRecord<1){ ?>
-
-                    <tr>
-                        <td colspan="2">No listed space.</td>
-                        
-                    
-                    </tr>
-
-            <?php    }
-        ?>
-       
+                    //check if there is space listed by owner. if no then echo 'no space listed' in table row
+                    $countRecord="SELECT * FROM space WHERE ownerID=$ownerID";
+                    $countRecord= $pdo->query($countRecord);
+                    $countRecord=$countRecord->rowCount();
+            
+                    if($countRecord<1){ ?>
+                        <tr>
+                            <td colspan="2">No listed space.</td>
+                        </tr>
+                <?php }
+                ?>
+            </tbody>
         </table>
-
         <br><br><br>
 
-        <h1>Add Calander Here</h1>
         <div id="calendarSection">
             <div id="calendar"></div>
         </div>
-        </section>
+    </section>
+    <br><br><br>
 
     <footer id="footer"></footer> 
 
@@ -164,6 +142,7 @@
                     everyYear: true // Same event every year (optional)
                 },
                 {
+                    id: 'bHay68d', // Event's ID (required)
                     name: "Vacation Leave",
                     badge: "02/13 - 02/15", // Event badge (optional)
                     date: ["February/13/2022", "February/15/2022"], // Date range
